1e95661b1d874d743744d968203a7377f7e57f2e
Alex LaFroscia <alex@lafroscia.com>
2019-01-15 23:02:21 -0800

    fix(accessibility): allow users to zoom into page

--- src/editorial/index.jsx
+++ src/editorial/index.jsx
@@ -7,10 +7,7 @@ export default ({ sidebar, children }) => (
   <>
     <Head>
       <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, user-scalable=no"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <div id="wrapper">
       <div id="main">
