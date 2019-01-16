a62d00006008fc2105838ec7439f01b8b01c851b
Alex LaFroscia <alex@lafroscia.com>
2019-01-15 22:48:57 -0800

    fix(accessibility): improve accessibility of icon component

--- src/editorial/Header.jsx
+++ src/editorial/Header.jsx
@@ -2,13 +2,23 @@ import React from "react";
 
 import ExternalLink from "../components/ExternalLink";
 
-export const Icon = ({ href, icon, children }) => (
-  <li>
-    <ExternalLink href={href} className={`icon fa-${icon}`}>
-      <span className="label">{children}</span>
-    </ExternalLink>
-  </li>
-);
+export const Icon = ({ href, icon, children }) => {
+  const id = "icon-" + icon + "-" + Math.floor(Math.random() * 1000000);
+
+  return (
+    <li>
+      <ExternalLink
+        href={href}
+        className={`icon fa-${icon}`}
+        aria-labelledby={id}
+      >
+        <label id={id} className="label">
+          {children}
+        </label>
+      </ExternalLink>
+    </li>
+  );
+};
 
 export const Logo = ({ href = "/", children }) => (
   <a href={href} className="logo">
