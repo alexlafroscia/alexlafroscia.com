6c92d86e013c13b87ac4456e5532eaa2b55d41dc
Alex LaFroscia <alex@lafroscia.com>
2019-01-15 23:26:56 -0800

    fix(seo): add meta tag for `content-type`

--- src/editorial/index.jsx
+++ src/editorial/index.jsx
@@ -7,6 +7,7 @@ export default ({ sidebar, children }) => (
   <>
     <Head>
       <meta charSet="utf-8" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <div id="wrapper">
