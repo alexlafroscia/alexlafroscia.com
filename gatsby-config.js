e37cc57e202cc4e671735a5099a5405fdc31ba82
Alex LaFroscia <alex@lafroscia.com>
2019-01-15 23:13:54 -0800

    chore: add `emotion`

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
