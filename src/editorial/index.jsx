266c1b224ba92d245395cd8d4649deb5d1e1d95d
Alex LaFroscia <alex@lafroscia.com>
2019-05-08 23:15:04 -0700

    fix: only initialize FontAwesome within Editorial component

--- src/editorial/index.jsx
+++ src/editorial/index.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import { Helmet as Head } from "react-helmet";
+import { Global } from "@emotion/core";
 
 import "./assets/css/main.css";
 
-export default ({ sidebar, children }) => (
-  <>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    <div id="wrapper">
-      <div id="main">
-        <div className="inner">{children}</div>
-      </div>
+import {
+  core as fontAwesomeCore,
+  usedIcons as fontAwesomeIcons
+} from "./global-styles/font-awesome";
 
-      {sidebar ? sidebar() : undefined}
-    </div>
-  </>
-);
+export default ({ sidebar, children }) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i|Roboto+Slab:400,700"
+          rel="stylesheet"
+        />
+      </Head>
+      <Global styles={fontAwesomeCore} />
+      <Global styles={fontAwesomeIcons} />
+      <div id="wrapper">
+        <div id="main">
+          <div className="inner">{children}</div>
+        </div>
+
+        {sidebar ? sidebar() : undefined}
+      </div>
+    </>
+  );
+};
