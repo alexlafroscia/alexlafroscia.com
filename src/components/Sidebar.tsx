6a4fe4d1c8dfff647a4479a62e967f4659aea8a2
Alex LaFroscia <alex@lafroscia.com>
2019-05-19 11:46:58 -0700

    feat(main): read Social Media links from YAML
    - Pull data out of TSX files
    - Add link to LinkedIn profile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,71 +1,89 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-import Sidebar, { Header, Nav, Section, Footer } from "../editorial/Sidebar";
+import EditorialSidebar, {
+  Header,
+  Nav,
+  Section,
+  Footer
+} from "../editorial/Sidebar";
 import LinkBase from "../elements/a";
 import { sanSerif } from "../theme/font";
+import { useStaticQuery, graphql } from "gatsby";
 
 const Link = styled(LinkBase)`
   font-family: ${sanSerif};
 `;
 
-export default () => (
-  <Sidebar>
-    <Nav>
-      <Header>Menu</Header>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/resume">Resume</Link>
-        </li>
-        <li>
-          <Link href="/articles/1">All Posts</Link>
-        </li>
-      </ul>
-    </Nav>
+const Sidebar = () => {
+  const social = useStaticQuery(graphql`
+    {
+      allSocialYaml {
+        edges {
+          node {
+            name
+            icon
+            link
+          }
+        }
+      }
+    }
+  `);
 
-    <Section title="Find Me Online">
-      <ul className="contact">
-        <li className="icon-github">
-          <Link newTab href="https://github.com/alexlafroscia">
-            Github
-          </Link>
-        </li>
-        <li className="icon-twitter">
-          <Link newTab href="https://twitter.com/alexlafroscia">
-            Twitter
-          </Link>
-        </li>
-        <li className="icon-medium">
-          <Link newTab href="https://medium.com/@alexlafroscia">
-            Medium
-          </Link>
-        </li>
-      </ul>
-    </Section>
+  return (
+    <EditorialSidebar>
+      <Nav>
+        <Header>Menu</Header>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/resume">Resume</Link>
+          </li>
+          <li>
+            <Link href="/articles/1">All Posts</Link>
+          </li>
+        </ul>
+      </Nav>
 
-    <Footer>
-      {Contribution => (
-        <>
-          <Contribution />
-          <br />
-          Colors from{" "}
-          <Link newTab href="https://aka.ms/nightowl">
-            Night Owl
-          </Link>{" "}
-          by{" "}
-          <Link newTab href="https://sarahdrasnerdesign.com/">
-            Sarah Drasner
-          </Link>
-          <br />
-          Built with{" "}
-          <Link newTab href="https://www.gatsbyjs.org/">
-            Gatsby
-          </Link>
-        </>
-      )}
-    </Footer>
-  </Sidebar>
-);
+      <Section title="Find Me Online">
+        <ul className="contact">
+          {social.allSocialYaml.edges
+            .map(edge => edge.node)
+            .map(socialNode => (
+              <li key={socialNode.icon} className={`icon-${socialNode.icon}`}>
+                <Link newTab href={socialNode.link}>
+                  {socialNode.name}
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </Section>
+
+      <Footer>
+        {Contribution => (
+          <>
+            <Contribution />
+            <br />
+            Colors from{" "}
+            <Link newTab href="https://aka.ms/nightowl">
+              Night Owl
+            </Link>{" "}
+            by{" "}
+            <Link newTab href="https://sarahdrasnerdesign.com/">
+              Sarah Drasner
+            </Link>
+            <br />
+            Built with{" "}
+            <Link newTab href="https://www.gatsbyjs.org/">
+              Gatsby
+            </Link>
+          </>
+        )}
+      </Footer>
+    </EditorialSidebar>
+  );
+};
+
+export default Sidebar;
