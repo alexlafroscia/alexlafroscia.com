6bda80e6ef121ef6c7c4c2b663b6644dadd5f9ed
Alex LaFroscia <alex@lafroscia.com>
2021-11-14 10:27:12 -0500

    Expand async iterable utilities

--- src/lib/async-iter.test.ts
+++ src/lib/async-iter.test.ts
@@ -1,4 +1,4 @@
-import { collect } from "./async-iter";
+import { collect, filter, log as logEach, map } from "./async-iter";
 
 test("collect", async () => {
   async function* run() {
@@ -7,4 +7,46 @@ test("collect", async () => {
   }
 
   expect(await collect(run())).toEqual([1, 2]);
+});
+
+test("filter", async () => {
+  async function* run() {
+    yield Promise.resolve(1);
+    yield Promise.resolve(2);
+  }
+
+  const evenFilter = filter((num: number) => num % 2 === 0);
+  const onlyEven = evenFilter(run());
+
+  expect(await collect(onlyEven)).toEqual([2]);
+});
+
+test("log", async () => {
+  const log = jest.fn();
+  async function* run() {
+    yield Promise.resolve(1);
+    yield Promise.resolve(2);
+  }
+
+  const output = logEach(run(), { log });
+
+  // Output is correct
+  expect(await collect(output)).toEqual([1, 2]);
+
+  // Logged as expected
+  expect(log).toHaveBeenCalledTimes(2);
+  expect(log).toHaveBeenCalledWith(1);
+  expect(log).toHaveBeenCalledWith(2);
+});
+
+test("map", async () => {
+  async function* run() {
+    yield Promise.resolve(1);
+    yield Promise.resolve(2);
+  }
+
+  const doubleMapper = map((num: number) => num * 2);
+  const doubled = doubleMapper(run());
+
+  expect(await collect(doubled)).toEqual([2, 4]);
 });
--- src/lib/async-iter.ts
+++ src/lib/async-iter.ts
@@ -1,3 +1,5 @@
+import type { MaybePromise } from "./types";
+
 export async function collect<T>(gen: AsyncIterableIterator<T>): Promise<T[]> {
   const result: Array<T> = [];
 
@@ -6,4 +8,40 @@ export async function collect<T>(gen: AsyncIterableIterator<T>): Promise<T[]> {
   }
 
   return result;
+}
+
+export function filter<T>(
+  filterCallback: (item: T) => boolean
+): (input: AsyncIterableIterator<T>) => AsyncIterableIterator<T> {
+  return async function* (input) {
+    for await (const element of input) {
+      if (filterCallback(element)) {
+        yield element;
+      }
+    }
+  };
+}
+
+interface Logger {
+  log(item: unknown): void;
+}
+
+export async function* log<T>(
+  input: AsyncIterableIterator<T>,
+  logger: Logger = console
+): AsyncIterableIterator<T> {
+  for await (const element of input) {
+    logger.log(element);
+    yield element;
+  }
+}
+
+export function map<T, U>(
+  mapCallback: (item: T) => MaybePromise<U>
+): (input: AsyncIterableIterator<T>) => AsyncIterableIterator<U> {
+  return async function* (input) {
+    for await (const element of input) {
+      yield await mapCallback(element);
+    }
+  };
 }
