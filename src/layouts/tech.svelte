f1cf15ae910b0e348930f2eddb69528c99d4b737
Alex LaFroscia <alex@lafroscia.com>
2021-11-14 13:14:56 -0500

    Better article styling

--- src/layouts/tech.svelte
+++ src/layouts/tech.svelte
@@ -7,8 +7,40 @@
   export let title = "";
 </script>
 
-{#if title}
-  <h1>{title}</h1>
-{/if}
+<article>
+  {#if title}
+    <h1>{title}</h1>
+  {/if}
 
-<slot />
+  <slot />
+</article>
+
+<style>
+  article {
+    /* Ensure article is a comfortable width */
+    width: 80ch;
+    max-width: 100%;
+    margin: 0 auto;
+  }
+
+  article > :global(p) {
+    line-height: 1.5rem;
+  }
+
+  article > :global(p) > :global(img) {
+    /* Ensure images are only as wide as the text */
+    max-width: 100%;
+  }
+
+  article > :global(pre) {
+    border-radius: 4px;
+    padding: 1rem;
+
+    /* Ensure code snippets do not increase max page width */
+    max-width: 100%;
+    overflow-x: auto;
+
+    /* Use "secondary" background color for code blocks */
+    background: var(--background-secondary);
+  }
+</style>
