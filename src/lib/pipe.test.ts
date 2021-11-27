b47b9e79e86ad51bf67a9c88bd741a6012480207
Alex LaFroscia <alex@lafroscia.com>
2021-11-27 13:26:46 -0500

    Fix formatting

--- src/lib/pipe.test.ts
+++ src/lib/pipe.test.ts
@@ -4,7 +4,7 @@ test("piping a value through multiple synchronous functions", async () => {
   const result = await pipe(
     1,
     (v) => v + 1,
-    (v) => v * 2,
+    (v) => v * 2
   );
 
   expect(result).toBe(4);
@@ -14,7 +14,7 @@ test("piping a value through multiple asynchronous functions", async () => {
   const result = await pipe(
     1,
     (v) => Promise.resolve(v + 1),
-    (v) => Promise.resolve(v * 2),
+    (v) => Promise.resolve(v * 2)
   );
 
   expect(result).toBe(4);
