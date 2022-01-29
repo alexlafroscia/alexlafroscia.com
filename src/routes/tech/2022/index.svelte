a763c6c35bde1edb9ed207ffc325c31920d9909c
Alex LaFroscia <alex@lafroscia.com>
2022-01-29 10:47:54 -0500

    Add index for 2022 posts

---
+++ src/routes/tech/2022/index.svelte
@@ -0,0 +1,32 @@
+<script context="module" lang="ts">
+  import { Post } from "$lib/post";
+  import { PostList } from "$lib/components";
+
+  export async function load({ fetch }) {
+    const posts = await Post.fetchAll(fetch);
+
+    return {
+      props: {
+        posts: posts
+          .filter((post) => post.slug.includes("2022"))
+          .sort(Post.compare)
+          .reverse(),
+      },
+    };
+  }
+
+  /**
+   * Pre-render the page at build-time
+   */
+  export const prerender = true;
+</script>
+
+<script lang="ts">
+  export let posts: Post[];
+</script>
+
+<main class="space-y-4 w-readable max-w-full mx-auto">
+  <h1 class="font-bold text-3xl">2022 Tech Posts</h1>
+
+  <PostList {posts} />
+</main>
