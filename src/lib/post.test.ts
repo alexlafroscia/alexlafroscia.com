6ae0d660158766ee4b174b6c1ec71a51c32fe17f
Alex LaFroscia <alex@lafroscia.com>
2022-09-18 16:53:14 -0400

    get the tests passing again

--- src/lib/post.test.ts
+++ src/lib/post.test.ts
@@ -1,5 +1,7 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { Post, type SerializedPost } from "./post";
+
+vi.mock("$app/environment", () => ({ browser: false }));
 
 describe("compare", () => {
   it("works with Post", () => {
