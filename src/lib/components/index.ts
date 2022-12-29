c0d2c446d8f4d497efe3b0f5a1b2e84d53bec3a2
Alex LaFroscia <alex@lafroscia.com>
2022-12-29 14:13:32 -0500

    sort imports

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Header } from "./Header.svelte";
-export { default as Sidebar } from "./Sidebar.svelte";
 export { default as Post } from "./Post.svelte";
 export { default as PostList } from "./PostList.svelte";
+export { default as Sidebar } from "./Sidebar.svelte";
