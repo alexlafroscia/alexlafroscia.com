0fb1dced244daa74a75915dca05d9385bea876ce
Alex LaFroscia <alex@lafroscia.com>
2023-06-10 09:23:19 -0400

    add index page for 2023 tech posts

---
+++ src/routes/tech/2023/+page.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+  import { PostList } from "$lib/components";
+</script>
+
+<script lang="ts">
+  import type { PageData } from "../2022/$types";
+
+  export let data: PageData;
+
+  const { posts } = data;
+</script>
+
+<svelte:head>
+  <title>2023 Tech Posts</title>
+</svelte:head>
+
+<main class="space-y-4 w-readable max-w-full mx-auto">
+  <h1 class="font-bold text-3xl">2023 Tech Posts</h1>
+
+  <PostList {posts} />
+</main>
