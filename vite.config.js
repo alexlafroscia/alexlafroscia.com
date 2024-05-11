6ce870a6bfe3ec84f8046724f04fdfcaff4bcf04
Alex LaFroscia <alex@lafroscia.com>
2024-05-11 17:37:02 -0400

    remove `vite-tsconfig-paths`

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vitest/config";
 import { sveltekit } from "@sveltejs/kit/vite";
 import rollupPluginYaml from "@rollup/plugin-yaml";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [sveltekit(), rollupPluginYaml(), tsconfigPaths()],
+  plugins: [sveltekit(), rollupPluginYaml()],
 
   define: {
     "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
