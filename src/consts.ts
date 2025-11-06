256d723bd56f6ba5f1c6004e252c7b182619eb2a
Alex LaFroscia <alex@lafroscia.com>
2025-11-06 17:05:22 -0500

    update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
+export const SITE_TITLE = "Alex LaFroscia";
 export const SITE_DESCRIPTION =
     "Hiker, weight lifter, archer, board gamer and cook. Sometimes a programmer, too. Probably walking my dog right now.";
