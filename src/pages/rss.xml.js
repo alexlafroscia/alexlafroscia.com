a037928df23348502163d4505b2f17bfae444b5f
Alex LaFroscia <alex@lafroscia.com>
2025-11-06 17:15:32 -0500

    expose full post contents to RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,22 @@ import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
     const posts = await getCollection("posts");
+
     return rss({
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
         site: context.site,
-        items: posts.map((post) => ({
-            ...post.data,
-            link: `/blog/${post.id}/`,
-        })),
+        items: posts.map((post) => {
+            const { data, id, rendered } = post;
+            const { title, description, date } = data;
+
+            return {
+                title,
+                description,
+                pubDate: date,
+                content: rendered.html,
+                link: `/${id}/`,
+            };
+        }),
     });
 }
